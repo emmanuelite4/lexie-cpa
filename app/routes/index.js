f7ae7d5585ecb7319a12a831351e05873d680d90
Emmanuel Alegbeleye <emmanuelite4@gmail.com>
2022-10-22 12:48:32 +0000

    Added landing page and fetched photos from api

---
+++ app/routes/index.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+
+export default class IndexRoute extends Route {
+  async model() {
+    try{
+    let response = await fetch('https://picsum.photos/v2/list');
+    let parsed = await response.json();
+
+    return parsed;
+    } catch(e){
+        console.error(e)
+    }
+  }
+}
