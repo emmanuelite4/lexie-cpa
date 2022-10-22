14f38c9cdf852a4e1765eec2e66341210d7fb470
Emmanuel Alegbeleye <emmanuelite4@gmail.com>
2022-10-22 14:04:42 +0100

    Added bootstrap to project

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,13 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   async model() {
-    try{
-    let response = await fetch('https://picsum.photos/v2/list');
-    let parsed = await response.json();
+    try {
+      let response = await fetch('https://picsum.photos/v2/list');
+      let parsed = await response.json();
 
-    return parsed;
-    } catch(e){
-        console.error(e)
+      return parsed;
+    } catch (e) {
+      console.error(e);
     }
   }
 }
